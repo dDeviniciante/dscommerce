114fb740d68c503b429f4bbd392d67a852754064
Jonatas Souza <jonatasboy66@gmail.com>
2026-07-04 18:04:15 -0300

    Create project and database

---
+++ src/main/java/com/cursodevsuperior/dscommerce/entities/User.java
@@ -0,0 +1,79 @@
+package com.cursodevsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String phone, String name, String email, long id, String password, LocalDate birthDate) {
+        this.phone = phone;
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.password = password;
+        this.birthDate = birthDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/cursodevsuperior/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodevsuperior.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
